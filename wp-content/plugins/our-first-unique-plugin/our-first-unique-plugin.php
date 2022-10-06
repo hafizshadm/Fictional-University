250bc86bc9f6e01b38eb84a2d6f0a8d7a6dedf6f
Shad Muhammad <hafizshad.pk@gmail.com>
2022-10-06 12:41:28 +0500

    Post Info Plugin Stats functionality done

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -7,7 +7,8 @@
     Author URI: https://fiverr.com/aynadigital
 */
 
-class WordCountAndTimePlugin {
+class WordCountAndTimePlugin
+{
     function __construct()
     {
         add_action('admin_menu', array($this, 'adminPage'));
@@ -15,13 +16,14 @@ class WordCountAndTimePlugin {
         add_filter('the_content', array($this, 'ifWrap'));
     }
 
-    function ifWrap($content) {
-        if(
-            (is_main_query() AND is_single())
-            AND
+    function ifWrap($content)
+    {
+        if (
+            is_main_query() and is_single()
+            and
             (
-                get_option('wcp_word_count', '1') OR
-                get_option('wcp_char_count', '1') OR
+                get_option('wcp_word_count', '1') or
+                get_option('wcp_char_count', '1') or
                 get_option('wcp_read_time', '1')
             )
         ) {
@@ -30,10 +32,40 @@ class WordCountAndTimePlugin {
         return $content;
     }
 
-    function createHTML($content) {
+    function createHTML($content)
+    {
 
-        $html = '<h3>'. get_option('wcp_headline', 'Post Statistics') .'</h3><p>';
-        $wordCount = str_word_count(strip_tags($content));
+        $html = '<div class="post-stats-wrap"><h3>' . esc_html(get_option('wcp_headline', 'Post Statistics')) . '</h3><p>';
+
+        if (get_option('wcp_word_count', '1') or get_option('wcp_read_time', '1')) {
+            $wordCount = str_word_count(strip_tags($content));
+        }
+
+        if (get_option('wcp_word_count', '1')) {
+            $html .= "This post has " . $wordCount . " words.<br>";
+        }
+
+        if (get_option('wcp_char_count', '1')) {
+            $html .= "This post has " . strlen(strip_tags($content)) . " chars.<br>";
+        }
+
+        if (get_option('wcp_read_time', '1')) {
+            $html .= "This post has take about " . round($wordCount / 225) . " minute(s) to read.<br>";
+        }
+
+        $html .= '</p></div>';
+
+        $html .= '<style>
+            .post-stats-wrap {
+                background-color: #fff;
+                padding: 20px;
+                border: 0;
+                border-radius: 4px;
+                box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+                margin: 20px 0;
+            }
+        </style>';
+
         if (get_option('wcp_location', '0') == '0') {
             return $html . $content;
         }
@@ -41,7 +73,8 @@ class WordCountAndTimePlugin {
     }
 
 
-    function settings() {
+    function settings()
+    {
         // Creating section with the help of add_settings_field
         add_settings_section(
             'wcp_first_section', //creating section
@@ -154,27 +187,28 @@ class WordCountAndTimePlugin {
     }
 
     /** 
-    * function readTimeHTML() {
-    *  ?> 
-    *    <input type="checkbox" name="wcp_read_time" value="1" <?php checked(get_option('wcp_word_count'), '1'); ?> />
-    *    <?php
-    * }
+     * function readTimeHTML() {
+     *  ?> 
+     *    <input type="checkbox" name="wcp_read_time" value="1" <?php checked(get_option('wcp_word_count'), '1'); ?> />
+     *    <?php
+     * }
 
-    * function charCountHTML() {
-    *    ?> 
-    *    <input type="checkbox" name="wcp_char_count" value="1" <?php checked(get_option('wcp_word_count'), '1'); ?> />
-    *    <?php
-    *}
+     * function charCountHTML() {
+     *    ?> 
+     *    <input type="checkbox" name="wcp_char_count" value="1" <?php checked(get_option('wcp_word_count'), '1'); ?> />
+     *    <?php
+     *}
 
-    *function wordCountHTML() {
-    *    ?> 
-    *    <input type="checkbox" name="wcp_word_count" value="1" <?php checked(get_option('wcp_word_count'), '1'); ?> />
-    *    <?php
-    *}
-    */
+     *function wordCountHTML() {
+     *    ?> 
+     *    <input type="checkbox" name="wcp_word_count" value="1" <?php checked(get_option('wcp_word_count'), '1'); ?> />
+     *    <?php
+     *}
+     */
 
-    function sanitize_display_location($input) {
-        if($input != '0' AND $input != '1') {
+    function sanitize_display_location($input)
+    {
+        if ($input != '0' and $input != '1') {
             add_settings_error('wcp_location', 'wcp_location_error', 'Display location must be either beginning or end');
             return get_option('wcp_location', 1);
         }
@@ -182,32 +216,36 @@ class WordCountAndTimePlugin {
         return $input;
     }
 
-// Reuseable checkbox
-    function checkboxHTML($args) {
-        ?> 
+    // Reuseable checkbox
+    function checkboxHTML($args)
+    {
+?>
         <input type="checkbox" name="<?php echo $args['theName'] ?>" value="1" <?php checked(get_option($args['theName']), '1'); ?> />
-        <?php
-    }
-    
-
-    function headlineHTML() {
-        ?> 
-            <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline')); ?>">
-        <?php
+    <?php
     }
 
-    function locationHTML() {
-        ?>
-        
+
+    function headlineHTML()
+    {
+    ?>
+        <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline')); ?>">
+    <?php
+    }
+
+    function locationHTML()
+    {
+    ?>
+
         <select name="wcp_location">
             <option value="0" <?php selected(get_option('wcp_location'), '0'); ?>>Beginning of the post</option>
             <option value="1" <?php selected(get_option('wcp_location'), '1'); ?>>End of the post</option>
         </select>
-        
-        <?php
+
+    <?php
     }
 
-    function adminPage() {
+    function adminPage()
+    {
         add_options_page(
             'Word Count Setting', //Title of the page
             'Word Count', //Menu Title
@@ -216,9 +254,10 @@ class WordCountAndTimePlugin {
             array($this, 'ourHTML') //Call back function
         );
     }
-    
-    function ourHTML() {
-        ?>
+
+    function ourHTML()
+    {
+    ?>
 
         <div class="wrap">
             <h1>Word Count Settings</h1>
@@ -227,18 +266,16 @@ class WordCountAndTimePlugin {
                 /* 
                 must use group name in settings_fields, it will add all the hidden html values from wordpress like NONCE, ACTIONS, 
                 its sort of security for us
-                */    
-                settings_fields('wordcountplugin'); 
+                */
+                settings_fields('wordcountplugin');
                 do_settings_sections('word-count-setting');
                 submit_button();
                 ?>
             </form>
         </div>
 
-        <?php
+<?php
     }
 }
 
 $wordCountAndTimePlugin = new WordCountAndTimePlugin();
-
-
